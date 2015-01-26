1e86bd42e54d3a4c4517f89d46c50bd2fcd5beb7
Gwyn Jones <gjones@nationalarchives.gsi.gov.uk>
2015-01-26 16:40:13 +0000

    Addition of breadcrumb

--- includes/index-page-content.php
+++ includes/index-page-content.php
@@ -1,8 +1,10 @@
-<div class="col starts-at-full clr holding-box">
-    <div class="heading-holding-banner">
-        <h2><span><span>[Page title]</span></span></h2>
+<section class="row">
+    <div class="col starts-at-full clr holding-box">
+        <div class="heading-holding-banner">
+            <h2><span><span>[Page title]</span></span></h2>
+        </div>
+        <div class="breather">
+            <p>[Page content]</p>
+        </div>
     </div>
-    <div class="breather">
-        <p>[Page content]</p>
-    </div>
-</div>
+</section>
--- index.php
+++ index.php
@@ -4,6 +4,7 @@
     <body>
         <?php require_once('includes/header.php'); ?>
         <main id="page_wrap" class="container" role="main">
+            <?php require_once('includes/index-page-breadcrumb.php'); ?>
             <?php require_once('includes/index-page-content.php'); ?>
         </main>
         <?php require_once('includes/footer.php'); ?>
