5e28d5e3c1326872b72252e70d72833ac0e1d287
Gwyn Jones <gjones@nationalarchives.gsi.gov.uk>
2015-01-26 16:40:34 +0000

    Addition of breadcrumb

---
+++ includes/index-page-breadcrumb.php
@@ -0,0 +1,17 @@
+<section class="row" id="holds-breadcrumb">
+    <div class="col starts-at-full ends-at-half clr">
+        <nav id="breadcrumb">
+            <ul>
+                <li><a href="http://www.nationalarchives.gov.uk/">Home</a></li>
+                <li><a href="http://discovery.nationalarchives.gov.uk/" class="breadcrumbs">...</a></li>
+                <li><a href="http://discovery.nationalarchives.gov.uk/results/r?_q=Nelson" class="breadcrumbs">...</a> ...
+                </li>
+            </ul>
+        </nav>
+    </div>
+    <div class="col starts-at-full ends-at-half clr">
+        <?php
+            // To include Discovery-like page controls simply introduce them as nav#page-controls > ul > li > a
+        ?>
+    </div>
+</section>
